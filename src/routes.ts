f604920df6f193e66245250c84e7b79bb318239d
Andrew <andrew_arnita@hotmail.com>
2021-04-15 00:45:42 +0300

    Auto-generated commit

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { ImageDetailComponent } from './app/image-detail/image-detail.component';
-import { GalleryComponent } from './app/gallery/gallery.component';
-import { Routes } from '@angular/router';
-
-export const appRoutes:Routes = [
-    { path: "gallery", component: GalleryComponent},
-    { path: "image/:id", component: ImageDetailComponent},
-    {path: "", redirectTo:"/gallery", pathMatch:"full"},
-]
